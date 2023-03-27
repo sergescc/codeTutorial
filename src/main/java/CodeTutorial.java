848a7f5c9dbec9b56bb18d222aa0d47792e90296
Sergio Coronado <s.coronado.c@gmail.com>
2023-03-27 17:35:20 -0400

    Added file printer'

--- src/main/java/CodeTutorial.java
+++ src/main/java/CodeTutorial.java
@@ -1,5 +1,24 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class CodeTutorial {
     public static void main(String[] args) {
-        System.out.println("This is a test");
+
+        System.out.println("This is the first element:");
+        System.out.println(args[0]);
+        try (PrintWriter out = new PrintWriter("filename.txt")) {
+            out.println();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            stringBuilder.append(args[i]);
+            stringBuilder.append(" is index: " + i + " \n");
+        }
+
+        System.out.println(stringBuilder.toString());
+        System.out.println("Im Done!");
     }
 }
